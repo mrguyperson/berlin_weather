bd05bc7d164763e5f409845e1121da8eb8dedacb
mrguyperson <ted.w.hermann@gmail.com>
2025-04-30 15:40:07 +0200

    cleaned up targets pipeline

--- _targets.R
+++ _targets.R
@@ -1,42 +1,9 @@
 # Load packages required to define the pipeline:
 library(targets)
-# library(tarchetypes) # Load other packages as needed.
 
 # Set target options:
 tar_option_set(
-  packages = c("tidyverse", "openmeteo", "scales", "glue", "showtext", "ggborderline") # Packages that your targets need for their tasks.
-  # format = "qs", # Optionally set the default storage format. qs is fast.
-  #
-  # Pipelines that take a long time to run may benefit from
-  # optional distributed computing. To use this capability
-  # in tar_make(), supply a {crew} controller
-  # as discussed at https://books.ropensci.org/targets/crew.html.
-  # Choose a controller that suits your needs. For example, the following
-  # sets a controller that scales up to a maximum of two workers
-  # which run as local R processes. Each worker launches when there is work
-  # to do and exits if 60 seconds pass with no tasks to run.
-  #
-  #   controller = crew::crew_controller_local(workers = 2, seconds_idle = 60)
-  #
-  # Alternatively, if you want workers to run on a high-performance computing
-  # cluster, select a controller from the {crew.cluster} package.
-  # For the cloud, see plugin packages like {crew.aws.batch}.
-  # The following example is a controller for Sun Grid Engine (SGE).
-  # 
-  #   controller = crew.cluster::crew_controller_sge(
-  #     # Number of workers that the pipeline can scale up to:
-  #     workers = 10,
-  #     # It is recommended to set an idle time so workers can shut themselves
-  #     # down if they are not running tasks.
-  #     seconds_idle = 120,
-  #     # Many clusters install R as an environment module, and you can load it
-  #     # with the script_lines argument. To select a specific verison of R,
-  #     # you may need to include a version string, e.g. "module load R/4.3.2".
-  #     # Check with your system administrator if you are unsure.
-  #     script_lines = "module load R"
-  #   )
-  #
-  # Set other options as needed.
+  packages = c("tidyverse", "openmeteo") 
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
@@ -52,10 +19,6 @@ list(
     name = start_date,
     command = "1940-01-01"
   ),
-  # tar_target(
-  #   name = today,
-  #   command = today()
-  # ),
   tar_target(
     name = raw_data,
     command = get_raw_data(city, start_date, today)
@@ -84,17 +47,6 @@ list(
     name = new_records,
     command = get_new_records(history_with_calendar, this_year)
   ),
-  # tar_target(
-  #   name = plot,
-  #   command = make_plot(
-  #     history_with_calendar, 
-  #     this_year, 
-  #     heat_records, 
-  #     city, 
-  #     start_date,
-  #     today),
-  #   format = "file"
-  # ),
   tar_target(
     name = top_10_hottest,
     command = get_top_10_list(filtered_data, type = "hottest")
