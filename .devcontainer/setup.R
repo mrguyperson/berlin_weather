425cbf41b640aef2e8a38589b370d1ab2ce6e897
mrguyperson <ted.w.hermann@gmail.com>
2025-05-02 10:45:19 +0200

    small change to renv setup

--- .devcontainer/setup.R
+++ .devcontainer/setup.R
@@ -1,9 +1,3 @@
-
-# Initialize renv and install packages using pak + DESCRIPTION
-if (!file.exists("renv.lock")) {
-  renv::init()
-  # install.packages("pak", repos = sprintf("https://r-lib.github.io/p/pak/stable/%s/%s/%s", .Platform$pkgType, R.Version()$os, R.Version()$arch))
-  # renv::install("pak")
   options(
     # renv.config.pak.enabled = TRUE,
     repos = c(RSPM = "https://packagemanager.posit.co/cran/2025-04-22")
@@ -21,6 +15,11 @@ if (!file.exists("renv.lock")) {
     "gt",
     "leaflet"
   )
+
+
+# Initialize renv and install packages using pak + DESCRIPTION
+if (!file.exists("renv.lock")) {
+  renv::init()
   renv::install(pkgs)
   renv::snapshot()
 } else {
